a78f60d8a4476dc8f0a79ed3b851493979f696b2
Samarth Kulshrestha <samarthkul6579@gmail.com>
2025-02-10 22:37:05 +0530

    Create 04.js

---
+++ week-2/task-1/04.js
@@ -0,0 +1,8 @@
+// Write a javascript code that calculates the squares and cubes from 0 to 10 
+
+for (let i = 0; i <= 10; i++) {
+    let square = i * i;
+    let cube = i * i * i;
+    console.log("Number: " + i + " | Square: " + square + " | Cube: " + cube);
+  }
+  
